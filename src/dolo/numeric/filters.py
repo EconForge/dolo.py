2f64a9f8ecea702e9daca080e6c75f0893a4ddd9
Pablo Winant <pablo.winant@gmail.com>
2011-01-24 20:55:29 +0000

    Added basic Hodrick-Prescott filter.

---
+++ src/dolo/numeric/filters.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def hpfilter( x, lam=1600 ):
+    T = x.shape[-1]
+    Au = np.array([
+        [1, -2],
+        [-2, 5]
+    ])
+    Ad = np.array([
+        [5, -2],
+        [-2, 1]
+    ])
+    a = np.diag( np.ones( T )*6 )
+    b = np.diag( np.ones( T-1 )*(-4), 1 )
+    c = np.diag( np.ones( T-2 ), 2 )
+    d = np.diag( np.ones( T-1 )*(-4), -1 )
+    e = np.diag( np.ones( T-2 ), -2 )
+    M = a + b + c + d + e
+    M[0:2,0:2] = Au
+    M[-2:,-2:] = Ad
+    M *= lam
+    M += np.eye(T)
+    return np.linalg.solve(M,x)
