af651c37dc4312ca5656de31cc2e3044bf85a40a
Pablo Winant <pablo.winant@gmail.com>
2011-06-19 22:12:00 +0200

    Added Levenberg-Marquardt solver.

---
+++ src/dolo/numeric/solver.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def solver(fobj,x0,options={},method='fsolve',jac='default',verbose=False):
+    in_shape = x0.shape
+
+    ffobj = lambda x: fobj(x.reshape(in_shape)).flatten()
+
+    if jac=='precise':
+        from numdifftools import Jacobian
+        Dffobj = Jacobian(ffobj)
+    else:
+        Dffobj = MyJacobian(ffobj)
+
+    if method == 'fsolve':
+        import scipy.optimize as optimize
+        factor = options['factor']
+        factor = factor if factor else 1
+        [sol,infodict,ier,msg] = optimize.fsolve(ffobj,x0.flatten(),factor=factor,full_output=True,xtol=1e-10,epsfcn=1e-9)
+        if ier != 1:
+            print msg
+    elif method == 'broyden1':
+        import scipy.optimize as optimize
+        sol = optimize.anderson(ffobj,x0.flatten())
+    elif method == 'newton_krylov':
+        import scipy.optimize as optimize
+        sol = optimize.newton_krylov(ffobj,x0.flatten())
+    elif method == 'lmmcp':
+        from dolo.numeric.extern.lmmcp import lmmcp
+        lb = -np.inf*np.ones(len(x0.flatten()))
+        ub = np.inf*np.ones(len(x0.flatten()))
+        sol = lmmcp(ffobj,Dffobj,x0.flatten(),lb,ub,verbose=verbose,options=options)
+
+    return sol.reshape(in_shape)
+
+
+def MyJacobian(fun):
+    eps = 1e-10
+    def rfun(x):
+        n = len(x)
+        x0 = x.copy()
+        y0 = fun(x)
+        D = np.zeros( (len(y0),len(x0)) )
+        for i in range(n):
+            delta = np.zeros(len(x))
+            delta[i] = eps
+            y = fun(x+delta)
+            D[:,i] = (y - y0)/eps
+        return D
+    return rfun
