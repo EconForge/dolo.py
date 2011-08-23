6ce645291a616f919acedb0c4985b79e0f32959d
Pablo Winant <pablo.winant@gmail.com>
2011-08-23 23:43:50 +0200

    added yaml_import to commands

--- src/dolo/__init__.py
+++ src/dolo/__init__.py
@@ -5,6 +5,7 @@ from dolo.config import *
 from symbolic.symbolic import *
 
 from misc.modfile import *
+from misc.yamlfile import *
 
 from numeric.perturbations import solve_decision_rule
 from numeric.decision_rules import *
