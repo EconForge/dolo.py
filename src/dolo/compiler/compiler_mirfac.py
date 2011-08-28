1c4e15673d3685c2cbff155af1796f50be2878ff
Pablo Winant <pablo.winant@gmail.com>
2011-08-28 20:49:57 +0200

    Updated recs output to include model informations and derivatives up to 3 order

--- src/dolo/compiler/compiler_mirfac.py
+++ src/dolo/compiler/compiler_mirfac.py
@@ -288,170 +288,7 @@ def model(flag,s,x,ep,e,{param_names}):
         #f.write( text )
         #f.close()
 
-    def process_output_matlab_old(self,target='recs',with_parameters_values=True, with_solution=False):
-
-        if target == 'recs':
-            with_param_names = False
-        if target == 'compecon':
-            with_param_names = True
-
-        data = self.read_model()
-        dmodel = self.model
-        model = dmodel
-
-        f_eqs = data['f_eqs']
-        g_eqs = data['g_eqs']
-        h_eqs = data['h_eqs']
-        states_vars = data['states_vars']
-        controls = data['controls']
-        exp_vars = data['exp_vars']
-        inf_bounds = data['inf_bounds']
-        sup_bounds = data['sup_bounds']
-
-        sub_list = dict()
-        for i,v in enumerate(exp_vars):
-            sub_list[v] = 'ep(:,{0})'.format(i+1)
-
-        for i,v in enumerate(controls):
-            sub_list[v] = 'x(:,{0})'.format(i+1)
-
-        for i,v in enumerate(states_vars):
-            sub_list[v] = 's(:,{0})'.format(i+1)
-
-        for i,v in enumerate(dmodel.shocks):
-            sub_list[v] = 'e(:,{0})'.format(i+1)
-
-        for i,v in enumerate(dmodel.parameters):
-            sub_list[v] = 'p({0})'.format(i+1)
-
-
-
-
-        text = '''
-function [out1,out2,out3,out4] = {mfname}(flag,s,x,ep,e,{param_names});
-
-% p is the vector of parameters
-{param_def}
-
-switch flag
-
-case 'b';
-    n = size(s,1);
-{eq_bounds_block}
-
-case 'f';
-    n = size(s,1);
-{eq_fun_block}
-
-case 'g';
-    n = size(s,1);
-{state_trans_block}
-
-case 'h';
-    n = size(s,1);
-{exp_fun_block}
-
-case 'params';
-{params_values}
-
-case 'solution';
-{solution}
-
-
-end;
-'''
-
-        from dolo.compiler.compiler import DicPrinter
-
-        dp = DicPrinter(sub_list)
-
-        def write_eqs(eq_l,outname='out1'):
-            eq_block = '    {0} = zeros(n,{1});\n'.format(outname,len(eq_l))
-            for i,eq in enumerate(eq_l):
-                eq_block += '    {0}(:,{1}) = {2};\n'.format( outname,  i+1,  dp.doprint_matlab(eq,vectorize=True) )
-            return eq_block
-
-        def write_der_eqs(eq_l,v_l,lhs):
-            eq_block = '    {lhs} = zeros(n,{0},{1});\n'.format(len(eq_l),len(v_l),lhs=lhs)
-            eq_l_d = eqdiff(eq_l,v_l)
-            for i,eqq in enumerate(eq_l_d):
-                for j,eq in enumerate(eqq):
-                    s = dp.doprint_matlab( eq, vectorize=True )
-                    eq_block += '    {lhs}(:,{0},{1}) = {2}; %d eq {eq_n} w.r.t. {vname}\n'.format(i+1,j+1,s,lhs=lhs,eq_n=i+1,vname=str(v_l[j]) )
-            return eq_block
-
-        eq_bounds_block = write_eqs(inf_bounds)
-        eq_bounds_block += write_eqs(sup_bounds,'out2')
-
-        eq_f_block = write_eqs(f_eqs)
-        eq_f_block += write_der_eqs(f_eqs,controls,'out2')
-        eq_f_block += write_der_eqs(f_eqs,exp_vars,'out3')
-        eq_f_block += write_der_eqs(f_eqs,states_vars,'out4')
-
-        eq_g_block = write_eqs(g_eqs)
-        eq_g_block += write_der_eqs(g_eqs,controls,'out2')
-        eq_g_block += write_der_eqs(g_eqs,states_vars,'out3')
-
-        if target == 'recs':
-            eq_h_block = write_eqs(h_eqs)
-            eq_h_block += '    out2 = zeros(n,{0},{1});\n'.format(len(h_eqs),len(controls)) ### TODO write the proper terms
-            eq_h_block += write_der_eqs(h_eqs,states_vars,'out3')
-            eq_h_block += write_der_eqs(h_eqs,controls,'out4')
-        else:
-            eq_h_block = write_eqs(h_eqs)
-            eq_h_block += write_der_eqs(h_eqs,controls,'out2')
-            eq_h_block += write_der_eqs(h_eqs,states_vars,'out3')
-
-        if not with_param_names:
-            eq_h_block = 's=snext;\nx=xnext;\n'+eq_h_block
-
-        param_def = 'p = [ ' + str.join(',',[p.name for p in dmodel.parameters])  + '];'
-
-        if not with_parameters_values:
-            params_values = ''
-        else:
-            [y,x,params] = model.read_calibration()
-            params_values = 'out1 = [' + str.join(  ',', [ str( p ) for p in params] ) + '];'
-
-        if with_solution:
-            from dolo.misc.matlab import value_to_mat
-            [[s_ss,x_ss],[X,Y,Z],bounds] = self.perturbation_solution()
-            solution = \
-'''    sol = struct;
-       sol.s_ss = {s_ss};
-       sol.x_ss = {x_ss};
-       sol.X = {X};
-       sol.Y = {Y};
-       sol.Z = {Z};
-       out1 = sol;
-'''.format(
-    s_ss = value_to_mat(s_ss),
-    x_ss = value_to_mat(x_ss),
-    X = value_to_mat(X),
-    Y = value_to_mat(Y),
-    Z = value_to_mat(Z)
-)
-        else:
-            solution = ''
-
-
-        text = text.format(
-            eq_bounds_block = eq_bounds_block,
-            mfname = 'mf_' + model.fname,
-            eq_fun_block=eq_f_block,
-            state_trans_block=eq_g_block,
-            exp_fun_block=eq_h_block,
-            param_names= 'snext,xnext,' + (str.join(',',[p.name for p in dmodel.parameters]) if with_param_names  else 'p'),
-            param_def= param_def if with_param_names else '',
-            params_values = params_values,
-            solution = solution,
-        )
-
-        return text
-
-    def process_output_matlab(self,target='recs', with_parameters_values=True, with_solution=False):
-
-        with_param_names = False
+    def process_output_recs(self, solution_order=False, fname=None):
 
         data = self.read_model()
         dmodel = self.model
@@ -498,7 +335,7 @@ output = struct('F',1,'Js',0,'Jx',0,'Jsn',0,'Jxn',0,'Jz',0,'hmult',0);
 if nargin == 9
   output                     = catstruct(output,out);
   voidcell                   = cell(1,5);
-  [out1,out2,out3,out4,out5] = {trick};
+  [out1,out2,out3,out4,out5] = voidcell{{:}};
 else
   if nargout >= 2, output.Js = 1; end
   if nargout >= 3, output.Jx = 1; end
@@ -531,11 +368,8 @@ switch flag
   case 'e';
     warning('Euler equation errors not implemented in Dolo');
 
-  case 'params';
-{params_values}
-
-  case 'solution';
-{solution}
+  case 'model'; % informations about the model
+{model_info}
 
 end
 '''
@@ -641,44 +475,46 @@ end
 
         # param_def = 'p = [ ' + str.join(',',[p.name for p in dmodel.parameters])  + '];'
 
-        if not with_parameters_values:
-            params_values = ''
-        else:
-            [y,x,params] = model.read_calibration()
-            params_values = '    out1 = [' + str.join(  ',', [ str( p ) for p in params] ) + '];'
+        from dolo.misc.matlab import value_to_mat
+        
+        # read model informations
+        [y,x,params_values] = model.read_calibration()
+        #params_values = '[' + str.join(  ',', [ str( p ) for p in params] ) + '];'
+        vvs = model.variables
+        s_ss = [ y[vvs.index(v)] for v in model['variables_groups']['states'] ]
+        x_ss = [ y[vvs.index(v)] for v in model['variables_groups']['controls'] ]
 
-        if with_solution:
-            from dolo.misc.matlab import value_to_mat
-            [[s_ss,x_ss],[X,Y,Z],bounds] = self.perturbation_solution()
-            solution = \
-'''sol = struct;
-        sol.s_ss = {s_ss};
-        sol.x_ss = {x_ss};
-        sol.X = {X};
-        sol.Y = {Y};
-        sol.Z = {Z};
-        out1 = sol;
+        model_info = '''
+    mod = struct;
+    mod.s_ss = {s_ss};
+    mod.x_ss = {x_ss};
+    mod.params = {params_values};
 '''.format(
     s_ss = value_to_mat(s_ss),
     x_ss = value_to_mat(x_ss),
-    X = value_to_mat(X),
-    Y = value_to_mat(Y),
-    Z = value_to_mat(Z)
+    params_values = value_to_mat(params_values)
 )
-        else:
-            solution = ''
+        if solution_order:
+            from dolo.numeric.perturbations_to_states import approximate_controls
+
+            ZZ = approximate_controls(self.model,order=solution_order)
+            n_c = len(controls)
+            ZZ = [e[:n_c,...] for e in ZZ] # keep only control vars. (x) not expectations (h)
+
+            solution = "    mod.X = cell({0},1);\n".format(len(ZZ))
+            for i,zz in enumerate(ZZ):
+                solution += "    mod.X{{{0}}} = {1};\n".format(i+1,value_to_mat(zz))
+            model_info += solution
+        model_info += '    out1 = mod;\n'
 
         text = text.format(
             eq_bounds_block = eq_bounds_block,
-            mfname = 'mf_' + model.fname,
+            mfname = fname if fname else 'mf_' + model.fname,
             eq_fun_block=eq_f_block,
             state_trans_block=eq_g_block,
             exp_fun_block=eq_h_block,
-            param_names= 'snext,xnext,' + (str.join(',',[p.name for p in dmodel.parameters]) if with_param_names  else 'p'),
-#            param_def= param_def if with_param_names else '',
-            params_values = params_values,
-            solution = solution,
-            trick = 'voidcell{:}' # stupid workaround to avoid {} to be substituted
+#            solution = solution,
+            model_info = model_info
         )
 
         return text
@@ -697,5 +533,5 @@ if __name__ == "__main__":
     model = dynare_import('../../../examples/global_models/optimal_growth.mod')
     model.check()
     mfcp = MirFacCompiler(model)
-    print mfcp.process_output_matlab()
+    print mfcp.process_output_recs()
     #print mfcp.process_output_python()
