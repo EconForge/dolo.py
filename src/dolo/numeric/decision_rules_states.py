c75a841e4825799c3cab7fcc6308b2d6409a65d1
Pablo Winant <pablo.winant@gmail.com>
2011-10-09 15:52:37 +0200

    New class for decision rules (controls depending on states)

---
+++ src/dolo/numeric/decision_rules_states.py
@@ -0,0 +1,26 @@
+from numpy import tile, dot
+from dolo.numeric.tensor import mdot
+
+class CDR:
+    def __init__(self,l):
+        self.order = len(l) - 2
+        self.S_bar = l[0]
+        self.X_bar = l[1]
+        self.X_s = l[2]
+        if self.order >= 2:
+            self.X_ss = l[3]
+        if self.order >= 3:
+            self.X_sss = l[4]
+
+    def __call__(self,s):
+        n_s = s.shape[1]
+        ds = s - tile( self.S_bar, (n_s,1) ).T
+        choice =  dot(self.X_s, ds) +  tile( self.X_bar, (n_s,1) ).T
+        if self.order == 2:
+            for i in range(n_s):
+                choice[:,i] += mdot(self.X_ss,[ds[:,i],ds[:,i]]) / 2
+        if self.order == 3:
+            for i in range(n_s):
+                choice[:,i] += mdot(self.X_ss,[ds[:,i],ds[:,i]]) / 2
+                choice[:,i] += mdot(self.X_sss,[ds[:,i],ds[:,i],ds[:,i]]) / 6
+        return choice
