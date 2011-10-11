b3ccc4c1454af02dc730e68d2895b87758a20bc2
Pablo Winant <pablo.winant@gmail.com>
2011-10-11 20:15:26 +0200

    Introduced functions to compute residuals on global spaces.

--- src/dolo/compiler/compiler_global.py
+++ src/dolo/compiler/compiler_global.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from dolo.numeric.perturbations_to_states import simple_global_representation
 from dolo.compiler.compiling import compile_function_2
 
@@ -26,3 +28,91 @@ def model_functions(model,substitute_auxiliary=False):
     f = compile_function_2(f_eqs, args_f, ['s','x','snext','xnext','e'], parameters, 'f' )
 
     return [f,g]
+
+class GlobalCompiler:
+    def __init__(self,model,substitute_auxiliary=False):
+        self.model = model
+
+        [f,g] = model_functions(model,substitute_auxiliary=substitute_auxiliary)
+        self.f = f
+        self.g = g
+
+
+
+def deterministic_residuals(s, x, interp, f, g, parms):
+    n_x = x.shape[0]
+    n_g = x.shape[1]
+    interp.fit_values(x)
+    dummy_epsilons = np.zeros((n_x,n_g))
+    [snext] = g(s,x,dummy_epsilons,parms)[:1]
+    [xnext] = interp.interpolate(snext)[:1]
+    [val] = f(s,x,snext,xnext,dummy_epsilons,parms)[:1]
+    return val
+
+
+def stochastic_residuals(s, x, dr, f, g, parms, epsilons, weights):
+    n_draws = epsilons.shape[1]
+    [n_x,n_g] = x.shape
+    dr.fit_values(x)
+    ss = np.tile(s, (1,n_draws))
+    xx = np.tile(x, (1,n_draws))
+    ee = np.repeat(epsilons, n_g , axis=1)
+    [ssnext] = g(ss,xx,ee,parms)[:1]
+    [xxnext] = dr.interpolate(ssnext)[:1]
+    [val] = f(ss,xx,ssnext,xxnext,ee,parms)[:1]
+
+    res = np.zeros( (n_x,n_g) )
+    for i in range(n_draws):
+        res += weights[i] * val[:,n_g*i:n_g*(i+1)]
+    return res
+
+
+def step_residual(s, x):
+    from serial_operations import strange_tensor_multiplication as stm
+    ss = tile(s, (1,n_draws))
+    xx = tile(x, (1,n_draws))
+    ee = repeat(epsilons, n_g , axis=1)
+    [ssnext, g_ss, g_xx] = g(ss,xx,ee,parms)[:3]
+    [xxnext, xxold_ss] = interp.interpolate(ssnext)[:2]
+    [val, f_ss, f_xx, f_ssnext, f_xxnext] = f(ss,xx,ssnext,xxnext,ee,parms)[:5]
+    dval = f_xx + stm(f_ssnext, g_xx) + stm(f_xxnext, stm(xxold_ss, g_xx))
+
+    res = np.zeros( (n_x,n_g) )
+    for i in range(n_draws):
+        res += weights[i] * val[:,n_g*i:n_g*(i+1)]
+
+    dres = np.zeros( (n_x,n_x,n_g) )
+    for i in range(n_draws):
+        dres += weights[i] * dval[:,:,n_g*i:n_g*(i+1)]
+
+    dval = np.zeros( (n_x,n_g,n_x,n_g))
+    for i in range(n_g):
+        dval[:,i,:,i] = dres[:,:,i]
+
+    return [res, dval]
+
+#f = model_fun['f']
+#g = model_fun['g']
+def test_residuals(s,dr, f,g,parms, epsilons, weights):
+    n_draws = epsilons.shape[1]
+
+    n_g = s.shape[1]
+    x = dr(s)
+    n_x = x.shape[0]
+
+    ss = np.tile(s, (1,n_draws))
+    xx = np.tile(x, (1,n_draws))
+    ee = np.repeat(epsilons, n_g , axis=1)
+    
+    [ssnext] = g(ss,xx,ee,parms)[:1]
+    xxnext = dr(ssnext)
+    [val] = f(ss,xx,ssnext,xxnext,ee,parms)[:1]
+
+    errors = np.zeros( (n_x,n_g) )
+    for i in range(n_draws):
+        errors += weights[i] * val[:,n_g*i:n_g*(i+1)]
+
+    squared_errors = np.power(errors,2)
+    std_errors = np.sqrt( np.sum(squared_errors,axis=0) )
+    
+    return std_errors
