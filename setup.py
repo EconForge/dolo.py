e03f37062e4fcaf35b9a0a42961f1f61610dabc2
Pablo Winant <pablo.winant@gmail.com>
2011-12-05 20:18:47 +0100

    Cherry-picked setup.py from master.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.3-dev"
+
+setup(
+    name = "dolo",
+    version = __version__,
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+    scripts = ['src/bin/dolo-recs.py'],
+
+    author = "Pablo Winant",
+    author_email = "pablo.winant@gmail.com",
+    description = 'DSGE modelling library',
+    license = 'BSD',
+    url = 'http://code.google.com/p/dynare-python/',
+
+)
+
