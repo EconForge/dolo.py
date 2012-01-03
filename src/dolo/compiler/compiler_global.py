9704573ebf90c519d6682eca22db65eb8b2465c9
Pablo Winant <pablo.winant@gmail.com>
2012-01-03 21:46:13 +0100

    Many changes to compiler_global.

--- src/dolo/compiler/compiler_global.py
+++ src/dolo/compiler/compiler_global.py
@@ -1,7 +1,52 @@
 import numpy as np
+import warnings
 
 from dolo.numeric.perturbations_to_states import simple_global_representation
 from dolo.compiler.compiling import compile_function_2
+
+from dolo.compiler.compiler_functions import full_functions
+
+from dolo.numeric.serial_operations import serial_multiplication as smult
+
+class GlobalCompiler2:
+    def __init__(self,model,substitute_auxiliary=False, solve_systems=False):
+        self.model = model
+
+        [f,a,g] = full_functions(model)
+        self.__f = f
+        self.__a = a
+        self.__g = g
+
+    def g(self,s,x,e,p,derivs=True):
+        if not derivs:
+            a = self.__a(s,x,p,derivs=False)[0]
+            return self.__g(s,x,a,e,p,derivs=False)
+        else:
+            [a,a_s,a_x] = self.__a(s,x,p)
+            [g,g_s,g_x,g_a,g_e] = self.__g(s,x,a,e,p)
+            G = g
+            G_s = g_s + smult(g_a,a_s)
+            G_x = g_x + smult(g_a,a_x)
+            G_e = g_e
+            return [G,G_s,G_x,G_e]
+
+
+    def f(self, s, x, snext, xnext, e, p, derivs=True):
+        if not derivs:
+            a = self.__a(s,x,p,derivs=False)[0]
+            anext = self.__a(snext,xnext,p,derivs=False)[0]
+            return self.__f(s,x,snext,xnext,a,anext,p,derivs=False)
+        else:
+            [a,a_s,a_x] = self.__a(s,x,p)
+            [A,A_S,A_X] = self.__a(snext,xnext,p)
+            [f,f_s,f_x,f_S,f_X,f_a,f_A] = self.__f(s,x,snext,xnext,a,A,p)
+            F = f
+            F_s = f_s + smult(f_a,a_s)
+            F_x = f_x + smult(f_a,a_x)
+            F_S = f_S + smult(f_A,A_S)
+            F_X = f_X + smult(f_A,A_X)
+            return [F,F_s,F_x,F_S,F_X]
+
 
 def model_functions(model,substitute_auxiliary=False, solve_systems=False):
     sgm = simple_global_representation(model,substitute_auxiliary=substitute_auxiliary, solve_systems=solve_systems)
@@ -39,11 +84,12 @@ class GlobalCompiler:
 
 
 
-def deterministic_residuals(s, x, interp, f, g, parms):
+def deterministic_residuals(s, x, interp, f, g, sigma, parms):
     n_x = x.shape[0]
     n_g = x.shape[1]
+    n_e = sigma.shape[0]
     interp.fit_values(x)
-    dummy_epsilons = np.zeros((n_x,n_g))
+    dummy_epsilons = np.zeros((n_e,n_g))
     [snext] = g(s,x,dummy_epsilons,parms)[:1]
     [xnext] = interp.interpolate(snext)[:1]
     [val] = f(s,x,snext,xnext,dummy_epsilons,parms)[:1]
@@ -79,9 +125,9 @@ def stochastic_residuals_2(s, theta, dr, f, g, parms, epsilons, weights, shape, 
     n_x = xx.shape[0]
 #    xx = np.tile(x, (1,n_draws))
     ee = np.repeat(epsilons, n_g , axis=1)
-    [SS, SS_ss, SS_xx, SS_ee] = g(ss, xx, ee, parms)
-    [XX, XX_SS, XX_t] = dr.interpolate(SS, with_theta_deriv=True)
-    [F, F_ss, F_xx, F_SS, F_XX, F_ee] = f(ss, xx, SS, XX, ee, parms)
+    [SS, SS_ss, SS_xx] = g(ss, xx, ee, parms)[:3]
+    [XX, XX_SS, XX_t] = dr.interpolate(SS, with_theta_deriv=True)[:3]
+    [F, F_ss, F_xx, F_SS, F_XX] = f(ss, xx, SS, XX, ee, parms)[:5]
 
 
     res = np.zeros( (n_x,n_g) )
@@ -102,6 +148,7 @@ def stochastic_residuals_2(s, theta, dr, f, g, parms, epsilons, weights, shape, 
 
 def stochastic_residuals_3(s, theta, dr, f, g, parms, epsilons, weights, shape, no_deriv=False):
 
+        import numpy
         n_t = len(theta)
         dr.theta = theta.copy().reshape(shape)
         #    [x, x_s, x_theta] = dr.interpolate(s, with_theta_deriv=True)
@@ -129,7 +176,7 @@ def stochastic_residuals_3(s, theta, dr, f, g, parms, epsilons, weights, shape, 
             dres += weights[i] * dF
         return [res,dres.swapaxes(1,2)]
 
-def step_residual(s, x, dr, f, g, parms, epsilons, weights, with_derivatives=True):
+def step_residual(s, x, dr, f, g, parms, epsilons, weights, x_bounds=None, serial_grid=True, with_derivatives=True):
     n_draws = epsilons.shape[1]
     [n_x,n_g] = x.shape
     from dolo.numeric.serial_operations import strange_tensor_multiplication as stm
@@ -139,6 +186,10 @@ def step_residual(s, x, dr, f, g, parms, epsilons, weights, with_derivatives=Tru
     if with_derivatives:
         [ssnext, g_ss, g_xx] = g(ss,xx,ee,parms)[:3]
         [xxnext, xxold_ss] = dr.interpolate(ssnext)[:2]
+        if x_bounds:
+            [lb,ub] = x_bounds(ssnext, parms)
+            xxnext = np.maximum(np.minimum(ub,xxnext),lb)
+
         [val, f_ss, f_xx, f_ssnext, f_xxnext] = f(ss,xx,ssnext,xxnext,ee,parms)[:5]
         dval = f_xx + stm(f_ssnext, g_xx) + stm(f_xxnext, stm(xxold_ss, g_xx))
 
@@ -150,23 +201,30 @@ def step_residual(s, x, dr, f, g, parms, epsilons, weights, with_derivatives=Tru
         for i in range(n_draws):
             dres += weights[i] * dval[:,:,n_g*i:n_g*(i+1)]
 
-#        dval = np.zeros( (n_x,n_g,n_x,n_g))
-#        for i in range(n_g):
-#            dval[:,i,:,i] = dres[:,:,i]
-
-        dval = dres
+        if not serial_grid:
+            dval = np.zeros( (n_x,n_g,n_x,n_g))
+            for i in range(n_g):
+                dval[:,i,:,i] = dres[:,:,i]
+        else:
+            dval = dres
 
         return [res, dval]
     else:
-        [ssnext] = g(ss,xx,ee,parms)[:1]
+        [ssnext] = g(ss,xx,ee,parms,derivs=False)[:1]
         [xxnext] = dr.interpolate(ssnext)[:1]
-        [val] = f(ss,xx,ssnext,xxnext,ee,parms)[:1]
+        if x_bounds:
+            [lb,ub] = x_bounds(ssnext, parms)
+            xxnext = np.maximum(np.minimum(ub,xxnext),lb)
+
+        [val] = f(ss,xx,ssnext,xxnext,ee,parms,derivs=False)[:1]
 
         res = np.zeros( (n_x,n_g) )
         for i in range(n_draws):
             res += weights[i] * val[:,n_g*i:n_g*(i+1)]
 
         return [res]
+
+
 #f = model_fun['f']
 #g = model_fun['g']
 def test_residuals(s,dr, f,g,parms, epsilons, weights):
@@ -194,16 +252,17 @@ def test_residuals(s,dr, f,g,parms, epsilons, weights):
     return std_errors
 
 
-def time_iteration(grid, interp, xinit, f, g, parms, epsilons, weights, options={}, verbose=True, method='lmmcp', maxit=500, debug=None, hook=None):
+def time_iteration(grid, interp, xinit, f, g, parms, epsilons, weights, x_bounds=None, options={}, serial_grid=True, verbose=True, method='lmmcp', maxit=500, nmaxit=50, backsteps=10, hook=None):
 
     from dolo.numeric.solver import solver
     from dolo.numeric.newton import newton_solver
 
-    fun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights)
-#    dfun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights)[1]
-
-    if debug:
-        maxit = debug
+    if serial_grid:
+        fun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights, x_bounds=x_bounds, with_derivatives=False)[0]
+        dfun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights, x_bounds=x_bounds)[1]
+    else:
+        fun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights, x_bounds=x_bounds, serial_grid=False, with_derivatives=False)[0]
+        dfun = lambda x: step_residual(grid, x, interp, f, g, parms, epsilons, weights, x_bounds=x_bounds, serial_grid=False)[1]
 
     #
     tol = 1e-8
@@ -213,27 +272,44 @@ def time_iteration(grid, interp, xinit, f, g, parms, epsilons, weights, options=
     err = 1
     x0 = xinit
     it = 0
-    if debug:
-        x_values = []
 
     verbit = True if verbose=='full' else False
+
+    if x_bounds:
+        [lb,ub] = x_bounds(grid,parms)
+    else:
+        lb = None
+        ub = None
+
+    if verbose:
+        s = "|\tIteration\t|\tStep\t\t\t|\tTime (s)\t\t|"
+        nnn = len(s.replace('\t',' '*4))
+        print('-'*nnn)
+        print(s)
+        print('-'*nnn)
+
+
 
     while err > tol and it < maxit:
         t_start = time.time()
         it +=1
         interp.fit_values(x0)
-    #    x = solver(fun, x0, method='lmmcp', jac='default', verbose=False, options=options)
-#        x = solver(fun, x0, method=method, jac=dfun, verbose=verbit, options=options)
-        x = newton_solver(fun,x0)
+
+        if serial_grid:
+            [x,nit] = newton_solver(fun,dfun,x0,lb=lb,ub=ub,infos=True, backsteps=backsteps, maxit=nmaxit)
+        else:
+            x = solver(fun, x0, lb=lb, ub=ub, method=method, jac=dfun, verbose=verbit, options=options)
+            nit = 0
+        # we restrict the solution to lie inside the boundaries
+        if x_bounds:
+            x = np.maximum(np.minimum(ub,x),lb)
 #        res = abs(fun(x)).max()
         err = abs(x-x0).max()
         t_finish = time.time()
         elapsed = t_finish - t_start
         if verbose:
-            print("iteration {} : {} : {}".format(it,err,elapsed))
+            print("\t\t{}\t|\t{}\t|\t{}\t|\t{}".format(it,err,elapsed,'-'*nit))
         x0 = x0 + (x-x0)
-        if debug:
-            x_values.append(x0)
         if hook:
             hook(interp,it,err)
         if False in np.isfinite(x0):
@@ -241,10 +317,7 @@ def time_iteration(grid, interp, xinit, f, g, parms, epsilons, weights, options=
             return [x0, x]
 
     if it == maxit:
-        if not debug:
-            raise("Maximum number of iterations reached")
-        else:
-            return x_values
+        warnings.warn(UserWarning("Maximum number of iterations reached"))
 
 
     t2 = time.time()
