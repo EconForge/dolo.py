81801ca044883247f9593926ea3d7a20613ada21
Pablo Winant <pablo.winant@gmail.com>
2012-01-12 11:56:39 +0100

    Added simulations function.

--- src/dolo/numeric/simulations.py
+++ src/dolo/numeric/simulations.py
@@ -1,114 +1,82 @@
-import numpy
-import numpy as np
 
-def multisimul(g, start, variance, horizon, n_exp, seed=1):
-    """
-    Computes multiple stochastic simulations.
-    @param g:
-    @param start:
-    @param variance:
-    @param horizon:
-    @param n_shocks:
-    @param seed:
-    @return: [mean values, standard deviations]
-    """
+def simulate(gc, dr, s0, sigma, n_exp, horizon, parms, seed=1, discard=True):
 
-    n_v = len(start)
-    S = variance
+    '''simulates series for a compiled model'''
 
-    epsilons = np.zeros( (n_v, horizon, n_exp) )
-    for i in range(n_exp):
-        epsilons[:,:,i] = numpy.random.multivariate_normal(M,S,horizon).T
+    if n_exp ==0:
+        irf = True
+        n_exp = 1
+    else:
+        irf = False
 
-    simul = np.zeros( (n_v,horizon + 1,n_exp) )
-    simul[:,0,:] = np.tile(start,(n_exp,1)).T
+    g = gc.g
 
-    for t in range(horizon):
-        simul[:,t+1,:] = g(simul[:,t,:], epsilons[:,t,:])
+    x0 = dr(s0)
+    a0 = gc.a(s0,x0, parms, derivs=False)[0]
 
-    sim = np.mean(simul,axis=2)
-    sd = np.std(simul,axis=2)
-    return [sim,sd]
+    s_simul = numpy.zeros( (s0.shape[0],n_exp,horizon) )
+    x_simul = numpy.zeros( (x0.shape[0],n_exp,horizon) )
+    a_simul = numpy.zeros( (a0.shape[0],n_exp,horizon) )
 
+    s_simul[:,:,0] = s0
+    x_simul[:,:,0] = x0
+    a_simul[:,:,0] = a0
 
-#####
+    for i in range(horizon):
+        mean = numpy.zeros(sigma.shape[0])
+        if irf:
+            epsilons = numpy.zeros( (sigma.shape[0],1) )
+        else:
+            seed += 1
+            numpy.random.seed(seed)
+            epsilons = numpy.random.multivariate_normal(mean, sigma, n_exp).T
+        s = s_simul[:,:,i]
+        x = dr(s)
+        x_simul[:,:,i] = x
+
+        a = gc.a(s,x,parms,derivs=False)[0]
+        a_simul[:,:,i] = a
+        ss = g(s,x,epsilons,parms,derivs=False)[0]
+
+        #for p in range(s0.shape[0]):
+        #    ss[p,:] = numpy.maximum( numpy.minimum(ss[p,:], large_bounds[1,p]), large_bounds[0,p])
+
+        if i<(horizon-1):
+            s_simul[:,:,i+1] = ss
+    from numpy import any,isnan,all
+
+    if discard:
+        iA = -isnan(s_simul)
+        valid = all( all( iA, axis=0 ), axis=1 )
+        [s_simul, x_simul, a_simul] = [ e[:,valid,:] for e in [s_simul, x_simul, a_simul] ]
+
+    return [s_simul, x_simul, a_simul]
+
 
 if __name__ == '__main__':
+    from dolo import yaml_import, approximate_controls
+    model = yaml_import('../../../examples/global_models/capital.yaml')
+    dr = approximate_controls(model)
 
-    print 'Running computations'
-    
-    A = np.array([[0.9,0],[0.0,0.9]])
-    B = np.eye(2)
-    M = np.zeros(2) # mean of innovations
-    S = np.eye(2)*0.005 # covariance matrix
-    def g(x,e):
-        return np.dot(A,x) + np.dot(B,e)
+    [y,x,parms] = model.read_calibration()
+    sigma = model.read_covariances()
 
-    n_v = 2
-    horizon = 50
-    n_exp = 100
-    seed = 1
-    start = np.array([1,0])
+    from dolo.compiler.compiler_global import GlobalCompiler2
 
-    import numpy.random
-    from scipy.stats.mstats import mquantiles
+    import numpy
+    gc = GlobalCompiler2(model)
+    s0 = numpy.atleast_2d( dr.S_bar ).T
 
-    import time
+    [s_simul, x_simul, a_simul] = simulate(gc, dr, s0, sigma, 10000, 50, parms)
 
 
-    # computations
-
-    print "\nComputing simulations using quantiles."
-    s = time.time()
-    
-    simul = np.zeros( (n_exp,n_v,horizon + 1) )
-    for k_exp in range(n_exp):
-        eps = numpy.random.multivariate_normal(M,S,horizon).T
-        sim = np.zeros((n_v,horizon+1))
-        sim[:,0] = start
-        for i in range(horizon):
-            sim[:,i+1] = g(sim[:,i],eps[:,i])
-        simul[k_exp,...] = sim
-
-    i_v = 0 # variable of interest
-
-    # compute quantiles
-    quants = [0.2,0.5,0.8]
-    n_q = len(quants)
-    out1 = np.zeros((n_q,horizon+1))
-    for t in range(horizon+1):
-        out1[:,t] = mquantiles(simul[:,i_v,t],quants)
-    #print out
-
-    ss = time.time()
-
-    print 'Elapsed : ' + str(ss - s)
-
-    # computations 2
-
-    print "\nComputing simulations using standard deviations."
-
-    import time
-    s = time.time()
-    [simul,sd] = multisimul(g, start, S, horizon, n_exp )
+    from matplotlib.pyplot import hist, show, figure
 
 
-    ss = time.time()
-    print 'Elapsed : ' + str(ss - s)
+    timevec = numpy.array(range(s_simul.shape[2]))
 
-    from matplotlib import pylab
-    x = np.linspace(0,horizon+1,num=horizon+1)
-    #pylab.plot(x,simul[0,i_v,:])
-
-
-    pylab.figure(1)
-    pylab.plot(x,out1[0,:],'-.',color='black')
-    pylab.plot(x,out1[1,:],color='black')
-    pylab.plot(x,out1[2,:],'-.',color='black')
-    
-
-    pylab.figure(2)
-    pylab.plot(x,simul[i_v,:],color='black')
-    pylab.plot(x,simul[i_v,:]-sd[i_v,:],'-.',color='black')
-    pylab.plot(x,simul[i_v,:]+sd[i_v,:],'-.',color='black')
-    pylab.show()
+    figure()
+    for i in range( 50 ):
+        hist( s_simul[0,:,i], bins=50 )
+        show()
+    #plot(timevec,s_simul[0,0,:])
