74d10ac9726821e2f1ee98f15de43f2df6a451f4
Pablo Winant <pablo.winant@gmail.com>
2012-04-30 10:01:13 +0200

    global_solve is now imported by default

--- src/dolo/__init__.py
+++ src/dolo/__init__.py
@@ -11,4 +11,5 @@ from numeric.perturbations import solve_decision_rule
 from numeric.decision_rules import *
 
 from dolo.numeric.perturbations_to_states import approximate_controls
+from dolo.numeric.global_solve import global_solve
 #from misc.commands import *
