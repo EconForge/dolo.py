d376f87578a545f8f7d7daf54a6e2cd0ed15a5e8
Pablo Winant <pablo.winant@gmail.com>
2012-09-08 13:01:42 +0200

    Removed some old files,

--- src/dolo/numeric/octave/create_space.m
+++
@@ -1,9 +0,0 @@
-function [interp] = create_space(tt, order, smin, smax)
-
-global interp;
-
-interp = struct;
-interp.fspace = fundefn(tt,order,smin,smax);       % function space
-interp.Phi    = funbasx(interp.fspace);
-interp.snodes = funnode(interp.fspace);                   % state collocation nodes
-interp.points = gridmake(interp.snodes);    % the grid on which the function is known
--- src/dolo/numeric/octave/set_coeffs.m
+++
@@ -1,6 +0,0 @@
-function [coeffs] = set_coeffs(values)
-
-    global interp;
-
-    coeffs = funfitxy( interp.fspace, interp.Phi, values );
-    interp.coeffs=coeffs;
