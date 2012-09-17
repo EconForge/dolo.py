bc56bfc11ebf0cceef79cc1df66bded00914cd58
Pablo Winant <pablo.winant@gmail.com>
2012-09-17 22:42:08 +0200

    Only one simulate functions for models with/without auxiliaries.

--- src/dolo/numeric/simulations.py
+++ src/dolo/numeric/simulations.py
@@ -1,84 +1,22 @@
 import numpy
 
-def simulate(gc, dr, s0, sigma, n_exp=0, horizon=40, parms=None, seed=1, discard=False):
+def simulate(cmodel, dr, s0, sigma, n_exp=0, horizon=40, with_auxiliaries=True, parms=None, seed=1, discard=False,):
 
-    '''simulates series for a compiled model'''
+    '''
+    :param cmodel: 
+    :param dr:
+    :param s0:
+    :param sigma:
+    :param n_exp:
+    :param horizon:
+    :param with_auxiliaries:
+    :param parms:
+    :param seed:
+    :param discard:
+    :return:
+    '''
 
-    gc = gc.as_type('fga')
-
-    if n_exp ==0:
-        irf = True
-        n_exp = 1
-    else:
-        irf = False
-
-#    from dolo.symbolic.model import Model
-#    if isinstance(gc,Model):
-#        from dolo.compiler.compiler_global import GlobalCompiler2
-#        model = gc
-#        gc = GlobalCompiler2(model)
-#        [y,x,parms] = model.read_calibration()
-
-    if parms == None:
-        parms = gc.model.read_calibration()[2]
-
-
-    s0 = numpy.atleast_2d(s0.flatten()).T
-
-    x0 = dr(s0)
-    a0 = gc.a(s0,x0, parms)
-
-    s_simul = numpy.zeros( (s0.shape[0],n_exp,horizon) )
-    x_simul = numpy.zeros( (x0.shape[0],n_exp,horizon) )
-    a_simul = numpy.zeros( (a0.shape[0],n_exp,horizon) )
-
-    s_simul[:,:,0] = s0
-    x_simul[:,:,0] = x0
-    a_simul[:,:,0] = a0
-
-    for i in range(horizon):
-        mean = numpy.zeros(sigma.shape[0])
-        if irf:
-            epsilons = numpy.zeros( (sigma.shape[0],1) )
-        else:
-            seed += 1
-            numpy.random.seed(seed)
-            epsilons = numpy.random.multivariate_normal(mean, sigma, n_exp).T
-        s = s_simul[:,:,i]
-        x = dr(s)
-        x_simul[:,:,i] = x
-
-        a = gc.a(s,x,parms)
-
-        a_simul[:,:,i] = a
-
-        ss = gc.g(s,x,a,epsilons,parms)
-
-        if i<(horizon-1):
-            s_simul[:,:,i+1] = ss
-    from numpy import any,isnan,all
-
-    simul = numpy.row_stack( [s_simul, x_simul, a_simul] )
-
-    if irf:
-        simul = simul[:,0,:]
-        return simul
-
-    if discard:
-        iA = -isnan(simul)
-        valid = all( all( iA, axis=0 ), axis=1 )
-        simul = simul[:,valid,:]
-        n_kept = simul.shape[1]
-        if n_exp > n_kept:
-            print( 'Discarded {}/{}'.format(n_exp-n_kept,n_exp))
-
-    return simul
-
-def simulate_without_aux(gc, dr, s0, sigma, n_exp=0, horizon=40, parms=None, seed=1, discard=False):
-
-    '''simulates series for a compiled model'''
-
-    gc = gc.as_type('fg')
+    cmodel = cmodel.as_type('fg')
 
     if n_exp ==0:
         irf = True
@@ -87,14 +25,14 @@ def simulate_without_aux(gc, dr, s0, sigma, n_exp=0, horizon=40, parms=None, see
         irf = False
 
     from dolo.symbolic.model import Model
-    if isinstance(gc,Model):
+    if isinstance(cmodel,Model):
         from dolo.compiler.compiler_global import GlobalCompiler
-        model = gc
-        gc = GlobalCompiler(model)
+        model = cmodel
+        cmodel = GlobalCompiler(model)
         [y,x,parms] = model.read_calibration()
 
     if parms == None:
-        parms = gc.model.read_calibration()[2]
+        parms = cmodel.model.read_calibration()[2]
 
 
     s0 = numpy.atleast_2d(s0.flatten()).T
@@ -119,14 +57,19 @@ def simulate_without_aux(gc, dr, s0, sigma, n_exp=0, horizon=40, parms=None, see
         x = dr(s)
         x_simul[:,:,i] = x
 
-        ss = gc.g(s,x,epsilons,parms)
+        ss = cmodel.g(s,x,epsilons,parms)
 
         if i<(horizon-1):
             s_simul[:,:,i+1] = ss
 
     from numpy import any,isnan,all
 
-    simul = numpy.row_stack([s_simul, x_simul])
+    if not with_auxiliaries:
+        simul = numpy.row_stack([s_simul, x_simul])
+    else:
+        a_simul = cmodel.a( s_simul.reshape((-1,n_exp*horizon)), s_simul.reshape( (-1,n_exp*horizon) ), parms)
+        a_simul = a_simul.reshape(-1,n_exp,horizon)
+        simul = numpy.row_stack([s_simul, x_simul, a_simul])
 
     if discard:
         iA = -isnan(x_simul)
@@ -135,11 +78,19 @@ def simulate_without_aux(gc, dr, s0, sigma, n_exp=0, horizon=40, parms=None, see
         n_kept = s_simul.shape[1]
         if n_exp > n_kept:
             print( 'Discarded {}/{}'.format(n_exp-n_kept,n_exp))
+
     if irf:
         simul = simul[:,0,:]
 
     return simul
 
+
+from dolo.misc.decorators import deprecated
+
+@deprecated
+def simulate_without_aux(cmodel, dr, s0, sigma, *args, **vargs):
+    vargs['with_auxiliaries'] = False
+    return simulate(cmodel, dr, s0, sigma, *args, **vargs)
 
 if __name__ == '__main__':
     from dolo import yaml_import, approximate_controls
@@ -152,19 +103,23 @@ if __name__ == '__main__':
     from dolo.compiler.compiler_global import GlobalCompiler2
 
     import numpy
-    gc = GlobalCompiler2(model)
+    cmodel = GlobalCompiler2(model)
     s0 = numpy.atleast_2d( dr.S_bar ).T
 
-    [s_simul, x_simul, a_simul] = simulate(gc, dr, s0, sigma, 10000, 50, parms)
+#    [s_simul, x_simul, a_simul] = simulate(cmodel, dr, s0, sigma, 10000, 50, parms, with_auxiliaries=True)
+    simul = simulate(cmodel, dr, s0, sigma, 10000, 50)
+    sim = simulate_without_aux(cmodel, dr, s0, sigma, 10000, 50)
+    print(simul.shape)
+    print sim.shape
 
 
     from matplotlib.pyplot import hist, show, figure
 
 
-    timevec = numpy.array(range(s_simul.shape[2]))
+    timevec = numpy.array(range(simul.shape[2]))
 
     figure()
     for i in range( 50 ):
-        hist( s_simul[0,:,i], bins=50 )
-        show()
+        hist( simul[0,:,i], bins=50 )
+    show()
     #plot(timevec,s_simul[0,0,:])
