40aa0a35e53930b525685456e9d0b8cce6890d17
Spencer Lyon <spencerlyon2@gmail.com>
2012-09-21 14:24:37 -0600

    Returned hpfilter to original version.
    Signed-off-by: Spencer Lyon <spencerlyon2@gmail.com>

--- src/dolo/numeric/filters.py
+++ src/dolo/numeric/filters.py
@@ -1,74 +1,35 @@
 import numpy as np
 from numpy import sin
-import scipy as sp
-import scipy.sparse.linalg as spla
 from scipy.signal import fftconvolve
 
 
-def hp_filter(data, lamb=1600):
-    """
-    This function will apply a Hodrick-Psomething filter to a dataset.
-    The return value is the filtered data-set found according to:
-        min sum((X[t] - T[t])**2 + lamb*((T[t+1] - T[t]) - (T[t] - T[t-1]))**2)
-          T   t
+def hpfilter( x, lam=1600 ):
+    T = x.shape[-1]
+    Au = np.array([
+        [1, -2],
+        [-2, 5]
+    ])
+    Ad = np.array([
+        [5, -2],
+        [-2, 1]
+    ])
+    a = np.diag( np.ones( T )*6 )
+    b = np.diag( np.ones( T-1 )*(-4), 1 )
+    c = np.diag( np.ones( T-2 ), 2 )
+    d = np.diag( np.ones( T-1 )*(-4), -1 )
+    e = np.diag( np.ones( T-2 ), -2 )
+    M = a + b + c + d + e
+    M[0:2,0:2] = Au
+    M[-2:,-2:] = Ad
+    M *= lam
+    M += np.eye(T)
 
-    T = Lambda**-1 * Y
-
-    Parameters
-    ----------
-        data: array, dtype=float
-            The data set for which you want to apply the HP_filter.
-            This mustbe a numpy array.
-        lamb: array, dtype=float
-            This is the value for lambda as used in the equation.
-
-    Returns
-    -------
-        T: array, dtype=float
-            The solution to the minimization equation above (the trend).
-        Cycle: array, dtype=float
-            This is the 'stationary data' found by Y - T.
-
-    Notes
-    -----
-        This function implements sparse methods to be efficient enough to handle
-        very large data sets.
-    """
-    Y = np.asarray(data)
-    if Y.ndim > 1:
-        Y = Y.squeeze()
-    lil_t = len(Y)
-    big_Lambda = sp.sparse.eye(lil_t, lil_t)
-    big_Lambda = sp.sparse.lil_matrix(big_Lambda)
-
-    # Use FOC's to build rows by group. The first and last rows are similar.
-    # As are the second-second to last. Then all the ones in the middle...
-    first_last = np.array([1 + lamb, -2 * lamb, lamb])
-    second = np.array([-2 * lamb, (1 + 5 * lamb), -4 * lamb, lamb])
-    middle_stuff = np.array([lamb, -4. * lamb, 1 + 6 * lamb, -4 * lamb, lamb])
-
-    #--------------------------- Putting it together --------------------------#
-
-    # First two rows
-    big_Lambda[0, 0:3] = first_last
-    big_Lambda[1, 0:4] = second
-
-    # Last two rows. Second to last first
-    big_Lambda[lil_t - 2, -4:] = second
-    big_Lambda[lil_t - 1, -3:] = first_last
-
-    # Middle rows
-    for i in range(2, lil_t - 2):
-        big_Lambda[i, i - 2:i + 3] = middle_stuff
-
-    # spla.spsolve requires csr or csc matrix. I choose csr for fun.
-    big_Lambda = sp.sparse.csr_matrix(big_Lambda)
-
-    T = spla.spsolve(big_Lambda, Y)
-
-    Cycle = Y - T
-
-    return T, Cycle
+    if x.ndim == 1:
+        return np.linalg.solve(M,x)
+    elif x.ndim > 3:
+        raise Exception('HP filter is not defined for dimension >= 3.')
+    else:
+        return np.linalg.solve(M,x.T).T
 
 
 def bandpass_filter(data, k, w1, w2):
