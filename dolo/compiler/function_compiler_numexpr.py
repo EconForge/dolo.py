5887ceafcfb384e7869a4b60eaacd9a555aef7cd
Pablo Winant <pablo.winant@gmail.com>
2013-01-30 14:29:10 +0000

    ENH: Derivatives in python compilers + numexpr code merge

--- dolo/compiler/function_compiler_numexpr.py
+++ dolo/compiler/function_compiler_numexpr.py
@@ -2,92 +2,13 @@ from __future__ import division
 
 from dolo.symbolic.derivatives import DerivativesTree
 from dolo.symbolic.symbolic import TSymbol
+from dolo.compiler.function_compiler import compile_multiargument_function as compile_multiargument_function_regular
+
 
 DerivativesTree.symbol_type = TSymbol
 
-def compile_multiargument_function(equations, args_list, args_names, parms, fname='anonymous_function', return_text=False):
-    """
-
-    :param equations: list of sympy expressions
-    :param args_list: list of lists of symbols (e.g. [[a_1,a_2], [b_1,b_2]])
-    :param args_names: list of strings (['a','b']
-    :param parms: list of symbols to be used as parameters
-    :param fname: name of the python function to be generated
-    :param diff: include symbolic derivates in generated function
-    :param vectorize: arguments are vectorized (not parameters)
-    :param return_function: if False, returns source code instead of a function
-    :return: a python function f(a,b,p) where p is a vector of parameters and a, b are stacked vectors (explain better)
-    """
-
-    template = '{0}_{1}'
-
-
-    declarations = ""
-
-    sub_list = {}
-    for i,args in enumerate(args_list):
-        vec_name = args_names[i]
-        for j,v in enumerate(args):
-            sub_list[v] = template.format(vec_name,j)
-            declarations += "    {0}_{1} = {0}[{1},...]\n".format(vec_name, j)
-
-    for i,p in enumerate(parms):
-        sub_list[p] = '{0}_{1}'.format('p',i)
-        declarations += "    {0}_{1} = {0}[{1}]\n".format('p', i)
-
-
-    text = '''
-def {fname}({args_names}, {param_names}, derivs=True):
-
-    import numexpr
-
-    from numpy import exp, log
-
-{declarations}
-
-    n = {var}.shape[-1]
-
-{content}
-
-    return {return_names}
-    '''
-
-    from dolo.compiler.common import DicPrinter
-
-    dp = DicPrinter(sub_list)
-
-    def write_eqs(eq_l,outname='val'):
-        eq_block = '    {0} = np.zeros( ({1},n) )\n'.format(outname, len(eq_l))
-        for i,eq in enumerate(eq_l):
-            #eq_block += "    {0}[{1},:] = numexpr.evaluate('{2}')\n".format(outname, i,  dp.doprint_numpy(str(eq)))
-            tt = dp.doprint(eq)
-            eq_block += "    {0}[{1},:] = numexpr.evaluate('{2}')\n".format(outname, i,  tt)
-        return eq_block
-
-
-    content = write_eqs(equations)
-
-    return_names = 'val'
-    text = text.format(
-            fname = fname,
-            declarations = declarations,
-            var = args_names[0],
-            content = content,
-            return_names = return_names,
-            args_names = str.join(', ', args_names),
-            param_names = 'p'
-            )
-
-    if return_text:
-        return text
-
-    import numpy as np
-    inf = np.inf
-    exec text in locals(), globals()
-    l = globals()
-    return l[fname]
-
-
+def compile_multiargument_function(equations, args_list, args_names, parms, fname='anonymous_function', diff=True, return_text=False):
+    return compile_multiargument_function_regular(equations, args_list, args_names, parms, fname=fname, diff=diff, return_text=return_text, use_numexpr=True)
 
 
 if __name__ == '__main__':
