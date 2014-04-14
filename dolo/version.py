4921a6b064fd53521b84d834db3d5114dbd5565d
Pablo Winant <pablo.winant@gmail.com>
2014-04-14 15:59:19 +0200

    Updated to experimental version number.

--- dolo/version.py
+++ dolo/version.py
@@ -1,2 +1,2 @@
-__version_info__ = ('0', '4', '6', '3')
+__version_info__ = ('0', '4', '7', '1')
 __version__ = '.'.join(__version_info__)
--- setup.py
+++ setup.py
@@ -17,49 +17,6 @@ setup(
 
     test_suite='dolo.tests',
 
-    cmdclass = {'build_ext': build_ext},
-
-    ext_modules = [
-
-        Extension(
-		    'dolo.numeric.interpolation.multilinear_cython',
-		    ['dolo/numeric/interpolation/multilinear_cython.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-
-        Extension(
-		    'dolo.numeric.interpolation.splines_filter',
-		    ['dolo/numeric/interpolation/splines_filter.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-        Extension(
-		    'dolo.numeric.interpolation.splines_cython',
-		    ['dolo/numeric/interpolation/splines_cython.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-        Extension(
-            'dolo.numeric.serial_operations_cython',
-            ['dolo/numeric/serial_operations_cython.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-        Extension(
-            'dolo.numeric.interpolation.splines_filter',
-            ['dolo/numeric/interpolation/splines_filter.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-        Extension(
-            'dolo.numeric.interpolation.splines_cython',
-            ['dolo/numeric/interpolation/splines_cython.pyx'],
-            extra_compile_args=['-O3']
-        ),
-
-    ],
-
     include_dirs = [np.get_include()],
     
     scripts = ['bin/dolo-recs', 'bin/dolo-matlab', 'bin/dolo-julia', 'bin/dolo'],
