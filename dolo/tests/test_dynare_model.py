509e740f3056821ac0791357a779acf438d7ab1a
Pablo Winant <pablo.winant@gmail.com>
2014-09-22 16:28:45 -0400

    FIX: removed warning in test_dynare_model

--- dolo/tests/test_dynare_model.py
+++ dolo/tests/test_dynare_model.py
@@ -1,16 +1,12 @@
+from dolo import *
+
+from dolo.algos.dynare.perturbations import *
+from dolo.algos.dynare.simulations import *
+
 def test_dynare_model():
 
-    from dolo import *
-
     model = yaml_import("examples/models/rbc_dynare.yaml")
-    from dolo.algos.dynare.perturbations import *
-
-    from dolo.algos.dynare.simulations import *
-
     dr = solve_decision_rule(model)
-
-
-    # sim = impulse_response_function(dr, 'e_z')
     sim = stoch_simul(dr, 'e_z', plot=False)
 
 if __name__ == '__main__':
