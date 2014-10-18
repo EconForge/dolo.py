ccefc7e0330620ce07d2940072c8aee6693ec25d
Pablo Winant <pablo.winant@gmail.com>
2014-10-18 18:36:12 -0400

    ENH: restored dynare models

---
+++ dolo/algos/dynare/__init__.py
@@ -0,0 +1,2 @@
+# from perturbations import solve_decision_rule
+# from simulations import impulse_response_function, stoch_simul
--- dolo/tests/test_dynare_model.py
+++ dolo/tests/test_dynare_model.py
@@ -6,8 +6,33 @@ from dolo.algos.dynare.simulations import *
 def test_dynare_model():
 
     model = yaml_import("examples/models/rbc_dynare.yaml")
-    dr = solve_decision_rule(model)
-    sim = stoch_simul(dr, 'e_z', plot=False)
+    dr = solve_decision_rule(model, order=2)
+    print(dr['ys'])
+
+    n_a = len(model.variables) + len(model.symbols['shocks'])
+
+    irf = simulate(dr, n_exp=0, start='risky')
+    assert(irf.shape == (41,n_a))
+    assert(irf.std().max()<0.00001)
+
+    irf = simulate(dr, n_exp=0, start='deterministic')
+    assert(irf.shape == (41,n_a))
+    assert(irf.std().max()>0.00001)
+
+    sim = simulate(dr, n_exp=1)
+    assert(sim.shape == (41,n_a))
+    assert(sim.std().max()>0.00001)
+
+    sim2 = simulate(dr, n_exp=10)
+    assert(sim2.shape == (10,41,n_a))
+
+    sim_shock = simulate(dr, n_exp=0, shock=[0.01])
+    print(sim_shock.keys())
+    from matplotlib import pyplot
+    pyplot.plot(sim_shock['z'])
+    # pyplot.plot(sim_shock['c'])
+    pyplot.show()
+
 
 if __name__ == '__main__':
 
