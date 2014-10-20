2fc2d30a8b8aea3048e665f4686f6331825e38bf
Pablo Winant <pablo.winant@gmail.com>
2014-10-20 15:59:57 -0400

    DOC: Added mock dependencies for readthedocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,26 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+
+try:
+    from mock import Mock as MagicMock
+except ImportError:
+    def install(package):
+        pip.main(['install', package])
+
+    install('mock')
+    from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['ipython', 'pyyaml', 'numpy', 'numba','numexpr','sympy','pandas','slycot','matplotlib']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
