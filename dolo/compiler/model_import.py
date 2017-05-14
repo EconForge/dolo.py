5d67431c94b4efcbbaac8e1fcee48cf2ee9e5fe3
Pablo Winant <pablo.winant@gmail.com>
2017-05-14 23:45:46 +0100

    Drastically simplified yaml_import.
    ...To the point where it is probably incorrect ;-)

--- dolo/compiler/model_import.py
+++ dolo/compiler/model_import.py
@@ -1,24 +1,11 @@
 import numpy
+import ruamel.yaml as ry
 
 from dolo.misc.display import read_file_or_url
 import yaml
 from collections import OrderedDict
 
-def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
-
-    class OrderedLoader(Loader):
-        pass
-    def construct_mapping(loader, node):
-        loader.flatten_mapping(node)
-        return object_pairs_hook(loader.construct_pairs(node))
-    OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
-    return yaml.load(stream, OrderedLoader)
-
-# usage example:
-
-def yaml_import(fname, return_symbolic=False, check=True, check_only=False):
+def yaml_import(fname, check=True, check_only=False):
 
     txt = read_file_or_url(fname)
 
@@ -33,98 +20,51 @@ def yaml_import(fname, return_symbolic=False, check=True, check_only=False):
 
     txt = txt.replace('^', '**')
 
-    return fast_import(txt, return_symbolic=return_symbolic, filename=fname)
+    data = ry.load(txt, ry.RoundTripLoader)
+    data['filename'] = fname
+
+    from dolo.compiler.model import Model
+
+    return Model(data)
 
 
-def autodetect_type(data):
-    if 'variables' in data['symbols']: return 'dynare'
-    elif 'exogenous' in data['symbols']: return 'dtcc'
-    else:
-        raise Exception("Could not detect model type")
+# This should be done in ModelSymbolic
+#
+# # all symbols are initialized to nan
+# # except shocks and markov_states which are initialized to 0
+# initial_values = {
+#     'shocks': 0,
+#     # 'markov_states': 0,
+#     'exogenous': 0,
+#     'expectations': 0,
+#     'values': 0,
+#     'controls': float('nan'),
+#     'states': float('nan')
+# }
+#
+# # variables defined by a model equation default to using these definitions
+# initialized_from_model = {
+#     'values': 'value',
+#     'expectations': 'expectation',
+#     'direct_responses': 'direct_response'
+# }
+#
+# for k, v in definitions.items():
+#     if k not in calibration:
+#         calibration[k] = v
+#
+# for symbol_group in symbols:
+#     if symbol_group not in initialized_from_model.keys():
+#         if symbol_group in initial_values:
+#             default = initial_values[symbol_group]
+#         else:
+#             default =  float('nan')
+#         for s in symbols[symbol_group]:
+#             if s not in calibration:
+#                 calibration[s] = default
 
 
-def fast_import(txt, return_symbolic=False, filename='<string>', parse_only=False):
 
-    import yaml
-    from dolo.compiler.language import minilang
-
-    for C in minilang:
-        k = C.__name__
-        yaml.add_constructor('!{}'.format(k), C.constructor)
-
-    txt = txt.replace('^', '**')
-
-    data = yaml.load(txt)
-    if parse_only:
-        return data
-
-    name = data['name']
-
-    model_type = data.get('model_type')
-    auto_type = autodetect_type(data)
-    if model_type is None:
-        model_type = auto_type
-    else:
-        assert(model_type == auto_type)
-
-
-    symbols = data['symbols']
-    definitions = data.get('definitions', {})
-    equations = data['equations']
-    calibration = data.get('calibration', {})
-    options = data.get('options', {})
-    domain = data.get('domain', {})
-    exogenous = data.get('exogenous', {})
-
-    infos = dict()
-    infos['filename'] = filename
-    infos['name'] = name
-    infos['type'] = model_type
-
-
-    # all symbols are initialized to nan
-    # except shocks and markov_states which are initialized to 0
-    initial_values = {
-        'shocks': 0,
-        # 'markov_states': 0,
-        'exogenous': 0,
-        'expectations': 0,
-        'values': 0,
-        'controls': float('nan'),
-        'states': float('nan')
-    }
-
-    # variables defined by a model equation default to using these definitions
-    initialized_from_model = {
-        'values': 'value',
-        'expectations': 'expectation',
-        'direct_responses': 'direct_response'
-    }
-
-    for k, v in definitions.items():
-        if k not in calibration:
-            calibration[k] = v
-
-    for symbol_group in symbols:
-        if symbol_group not in initialized_from_model.keys():
-            if symbol_group in initial_values:
-                default = initial_values[symbol_group]
-            else:
-                default =  float('nan')
-            for s in symbols[symbol_group]:
-                if s not in calibration:
-                    calibration[s] = default
-
-    from dolo.compiler.model_symbolic import SymbolicModel
-    smodel = SymbolicModel(name, model_type,
-                        symbols, equations, calibration,
-                        domain=domain, exogenous=exogenous,
-                        options=options, definitions=definitions)
-
-    from dolo.compiler.model_numeric import NumericModel
-    model = NumericModel(smodel, infos=infos)
-
-    return model
 
 
 if __name__ == "__main__":
