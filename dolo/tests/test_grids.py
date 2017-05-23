c3b555c44c45312203297dd9a59d3172db93a2af
Pablo Winant <pablo.winant@gmail.com>
2017-05-23 10:55:18 +0100

    .gitignore + test_grids.py

---
+++ dolo/tests/test_grids.py
@@ -0,0 +1,27 @@
+def test_grids():
+
+    from dolo.numeric.grids import CartesianGrid, UnstructuredGrid, NonUniformCartesianGrid, SmolyakGrid
+    from dolo.numeric.grids import n_nodes, nodes, node
+    print("Cartsian Grid")
+    grid = CartesianGrid([0.1, 0.3], [9, 0.4], [50, 10])
+    print(grid.nodes())
+    print(nodes(grid))
+
+    print("UnstructuredGrid")
+    ugrid = UnstructuredGrid([[0.1, 0.3], [9, 0.4], [50, 10]])
+    print(nodes(ugrid))
+    print(node(ugrid,0))
+    print(n_nodes(ugrid))
+
+
+    print("Non Uniform CartesianGrid")
+    ugrid = NonUniformCartesianGrid([[0.1, 0.3], [9, 0.4], [50, 10]])
+    print(nodes(ugrid))
+    print(node(ugrid,0))
+    print(n_nodes(ugrid))
+
+    print("Smolyak Grid")
+    sg = SmolyakGrid([0.1, 0.2], [1.0, 2.0], 2)
+    print(nodes(sg))
+    print(node(sg, 1))
+    print(n_nodes(sg))
