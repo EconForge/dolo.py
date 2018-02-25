b39024aa5afe6b3f685f454c8cdb5672169bc438
Pablo Winant <pablo.winant@gmail.com>
2018-02-25 18:19:48 +0100

    REF: removed unused dependencies.

--- setup.py
+++ setup.py
@@ -20,12 +20,11 @@ setup(
 
     scripts = ['bin/dolo-recs', 'bin/dolo-matlab', 'bin/dolo-julia', 'bin/dolo', 'bin/dolo-lint'],
 
-    install_requires = ["pyyaml", "numba", "numpy", "numexpr", "sympy","scipy","quantecon",
+    install_requires = ["pyyaml", "numba", "numpy", "sympy","scipy","quantecon",
                         "pandas", "interpolation", "ipython", "ruamel.yaml", "xarray"],
 
     extras_require = {
             'plots':  ["matplotlib"],
-            'higher_order': ["slycot"]
     },
 
     author = "Pablo Winant",
