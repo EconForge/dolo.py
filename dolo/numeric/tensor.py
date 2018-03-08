7a95233e940a32fdef2870425b1cdedefe5d99be
Pablo Winant <pablo.winant@gmail.com>
2018-03-08 19:36:05 +0000

    ENH: rewrote mdot using einsum.

--- dolo/numeric/tensor.py
+++ dolo/numeric/tensor.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def multidot(ten,mats):
+def multidot_old(ten,mats):
     '''
     Implements tensor operation : tensor-times-matrices.
     If last dimensions of ten represent multilinear operations of the type : [X1,...,Xk]->B[X1,...,Xk]
-    and mats contains matrices or vectors [A1,...Ak] the function returns an array representing operators : 
+    and mats contains matrices or vectors [A1,...Ak] the function returns an array representing operators :
     [X1,...,Xk]->B[A1 X1,...,Ak Xk]
     '''
     resp = ten
@@ -15,9 +15,31 @@ def multidot(ten,mats):
         resp = np.tensordot( resp, mats[i], (n_d-n_m,0) )
     return resp
 
-mdot = multidot
-#def mdot( U, V ):
-#    return multidot( U, V )
+# should consist in shapes only
+def mdot_signature(M, *C):
+  M_syms = [chr(97+e) for e in range(len(M))]
+  fC_syms = M_syms[-len(C):]
+  ic = 97+len(M_syms)
+  C_syms = []
+  for i in range(len(C)):
+    c_sym = [fC_syms[i]]
+    for j in range(len(C[i])-1):
+      c_sym.append(chr(ic))
+      ic += 1
+    C_syms.append(c_sym)
+  C_sig = [M_syms] + C_syms
+  out_sig = [M_syms[:-len(C)]] + [cc[1:] for cc in C_syms]
+  args = ','.join([''.join(g) for g in C_sig])
+  out = ''.join([''.join(g) for g in out_sig])
+
+  return args + '->' + out
+
+from numpy import einsum
+
+def mdot(M,*C):
+  sig = mdot_signature(M.shape,*[c.shape for c in C])
+  return einsum(sig, M, *C)
+
 
 def sdot( U, V ):
     '''
