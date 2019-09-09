85ca78798b559e5bbcd56e6860226aebfd072a5b
Pablo Winant <pablo.winant@gmail.com>
2019-09-09 12:05:45 +0200

    WIP: ergodic distribution (test).

--- dolo/algos/commands.py
+++ dolo/algos/commands.py
@@ -6,5 +6,6 @@ from dolo.algos.value_iteration import evaluate_policy, value_iteration
 from dolo.algos.improved_time_iteration import improved_time_iteration
 from dolo.algos.steady_state import residuals
 from dolo.algos.perturbation import perturb
+from dolo.algos.ergodic import ergodic_distribution
 
 approximate_controls = perturb
