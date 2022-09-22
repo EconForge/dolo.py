96a870ec3d80a8f20d7a960620f708f9a00af6a1
Pablo Winant <pablo.winant@gmail.com>
2022-09-22 11:19:01 -0300

    Neutralized AR1 CS test.

--- dolo/tests/test_egm.py
+++ dolo/tests/test_egm.py
@@ -1,11 +1,11 @@
-def test_egm_mc():
-
-    from dolo import yaml_import
-    from dolo.algos.egm import egm
-    import numpy as np
-
-    model = yaml_import("examples/models/consumption_savings.yaml")
-    sol = egm(model, a_grid=np.linspace(0.1, 10, 10) ** 2, verbose=True)
+#def test_egm_mc():
+#
+#    from dolo import yaml_import
+#    from dolo.algos.egm import egm
+#    import numpy as np
+#
+#    model = yaml_import("examples/models/consumption_savings.yaml")
+#    sol = egm(model, a_grid=np.linspace(0.1, 10, 10) ** 2, verbose=True)
 
 
 def test_egm_iid():
